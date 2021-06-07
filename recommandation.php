eda23ede213d76b98bc9ca47cb889ff44720b68e
LeviKaza <M.Jason.45@hotmail.fr>
2021-06-07 14:26:05 +0200

    add more recommandation

--- recommandation.php
+++ recommandation.php
@@ -13,14 +13,26 @@
         <div class="all">
             <div id="anime" class="display">
                 <p>Anime :</p>
-                <img src="img/fateUbwCover.jpg">
+                <a href="https://www.nautiljon.com/animes/akame+ga+kill!.html"><img src="img/akameGaKill.jpg"></a>
+                <a href="https://www.nautiljon.com/animes/ansatsu+kyoushitsu.html"><img src="img/assassinationClassroom.jpg"></a>
+                <a href="https://www.nautiljon.com/animes/citrus.html"><img src="img/citrus.jpg"></a>
+                <a href="https://www.nautiljon.com/animes/darker+than+black+-+kuro+no+keiyakusha.html"><img src="img/darkerThanBlack.jpg"></a>
+                <a href="https://www.nautiljon.com/animes/death+parade.html"><img src="img/deathParade.jpg"></a>
+                <a href="https://www.nautiljon.com/mangas/dusk+maiden+of+amnesia.html"><img src="img/duskMaidenOfAmnesia.jpg"></a>
+                <a href="https://www.nautiljon.com/animes/kyoukai+no+kanata.html"><img src="img/kyoukaiNoKanata.jpg"></a>
                 <?php if ($_SESSION['name'] === 'Yuri'): ?>
                     <input type="checkbox" onclick="viewAnime()">
                 <?php endif ?>
             </div>
             <div id="manga" class="display">
                 <p>Manga :</p>
-                <img src="img/slime.jpg">
+                <a href="https://www.nautiljon.com/mangas/artelier+collection.html"><img src="img/artelier.jpg"></a>
+                <a href="https://www.nautiljon.com/mangas/bloom+into+you.html"><img src="img/bloomIntoYou.jpg"></a>
+                <a href="https://www.nautiljon.com/mangas/komi-san+wa+commu-shou+desu..html"><img src="img/komiSan.jpg"></a>
+                <a href="https://www.nautiljon.com/mangas/hitomi-chan+wa+hito+mishiri.html"><img src="img/hitomiChan.jpg"></a>
+                <a href="https://www.nautiljon.com/mangas/the+ancient+magus+bride.html"><img src="img/mahouTsukaiNoYome.jpg"></a>
+                <a href="https://www.nautiljon.com/mangas/moi,+quand+je+me+r%C3%A9incarne+en+slime.html "><img src="img/tensura.jpg"></a>
+                <a href="https://www.nautiljon.com/mangas/tonikaku+kawaii.html"><img src="img/tonikakuKawai.jpg"></a>
                 <?php if ($_SESSION['name'] === 'Yuri'): ?>
                     <input type="checkbox" onclick="viewManga()">
                 <?php endif ?>
